f59fbce96db23b5fc03f07cfed12dea24741c01b
Tiago Romero <tiromero34@gmail.com>
2023-11-28 00:16:33 -0300

    removido contato e sobre

--- recipes/views.py
+++ recipes/views.py
@@ -1,12 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
     return render(request, 'recipes/home.html')
-
-def sobre(request):
-    return HttpResponse('Sobre')
-
-def contato(request):
-    return HttpResponse('Contato')
