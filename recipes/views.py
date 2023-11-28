99850748c9b02a3f773147cd1d56a8a9208b2c61
Tiago Romero <tiromero34@gmail.com>
2023-11-27 23:56:50 -0300

    exemplo de template temp

---
+++ recipes/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def home(request):
+    return render(request, 'recipes/home.html')
+
+def sobre(request):
+    return HttpResponse('Sobre')
+
+def contato(request):
+    return HttpResponse('Contato')
