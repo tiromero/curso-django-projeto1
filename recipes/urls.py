f1ad2b5304ec83d80e5dca7616d02f6cf21e8835
Tiago Romero <tiromero34@gmail.com>
2023-12-04 09:05:15 -0300

    models

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from . import views
 
+#recipes:recipe
+app_name = 'recipes'
+
 urlpatterns = [
-    path('', views.home), #Home
-    path('recipes/<int:id>/', views.recipe), 
+    path('', views.home, name="home"), #Home
+    path('recipes/<int:id>/', views.recipe, name="recipe"), 
 
 ]
