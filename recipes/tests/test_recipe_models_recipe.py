c973d6eb28532c271d655f500df27ec9b1909f2d
Tiago Romero <tiromero34@gmail.com>
2024-01-04 10:46:59 -0300

    ValidationError

--- recipes/tests/test_recipe_models_recipe.py
+++ recipes/tests/test_recipe_models_recipe.py
@@ -1,10 +1,13 @@
 from .test_recipe_base import RecipeTestBase
+from django.core.exceptions import ValidationError
 
 class RecipeModelTest(RecipeTestBase):
     def setUp(self) -> None:
         self.recipe = self.make_recipe()
         return super().setUp()
     
-    def test_the_test(self):
-        recipe = self.recipe
-        ...
+    def test_recipe_title_raises_error_if_title_has_more_than_65_chars(self):
+        self.recipe.title = 'A'* 70
+
+        with self.assertRaises(ValidationError):
+            self.recipe.full_clean() # Aqui a validação ocorre.
